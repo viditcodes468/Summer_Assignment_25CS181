7503f16d29d235ffcec9b52ae557191df5b33b55
vidit chauhan <viditchauhan425@gmail.com>
2026-06-11 20:33:05 +0530

    day 7 solutions

---
+++ q27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int n, i, j;
+printf("Enter number of rows: ");
+scanf("%d",&n);
+for(i=1; i<=n; i++) {
+for(j=0; j<i; j++) {
+printf("%c", 'A' + j);
+}
+printf("\n");
+}
+return 0;
+}
