326842d7f02169383b3f041e6cf7a8d15edc9067
vidit chauhan <viditchauhan425@gmail.com>
2026-06-12 20:23:43 +0530

    day 8 solutions

---
+++ q32.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int n, i, j;
+printf("Enter size of square: ");
+scanf("%d", &n);
+for(i = 1; i <= n; i++) {
+for(j = 1; j <= n; j++) {
+if(i == 1 || i == n || j == 1 || j == n)
+printf("*");
+else
+printf(" ");
+}
+printf("\n");
+}
+return 0;
+}
